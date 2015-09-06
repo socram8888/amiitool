3b4462948b1aa0532a8f12988c609fa9c191a9ee
Marcos Del Sol Vives <socram8888@gmail.com>
2015-09-06 19:50:03 +0200

    Added server and client for network de/encoding

---
+++ network/amiicli.sh
@@ -0,0 +1,125 @@
+#!/bin/bash -e
+
+fsize() {
+	wc -c "$1" | cut -f1 -d' '
+}
+
+gentoken() {
+	local LOGIN="$1"
+
+	# Auth version (1)
+	echo -n "1"
+
+	# Fixed part
+	echo -n "${LOGIN:0:16}"
+
+	# Time-based token
+	TICK=$(($(date -u +%s)/30))
+	echo -n "$TICK-${LOGIN:16:16}" | sha256sum | head -c16
+	echo -n "$TICK-${LOGIN:16:16}" >&2
+
+	echo "Tick: $TICK" >&2
+}
+
+usage() {
+	echo "Usage: $0 [e|d] (input)? (output)?" >&2
+	echo >&2
+	echo "If input or output are not specified, stdin and stdout will be used" >&2
+	echo "If input if specified but output is not, output will be input with .dec appended" >&2
+	exit 1
+}
+
+loadapikey() {
+	set +e
+	. ~/.amiibokey.sh
+	set -e
+	if ! [[ "${AMIIBO_API_KEY}" =~ ^[a-fA-F0-9]{32} ]]; then
+		echo "API key not found or invalid" >&2
+		echo >&2
+		echo "To prevent cheating and counterfeiting, this service is not public and you need a key before you may use it. You may request one by contating socram@protonmail.ch along with an explanation on why you need it." >&2
+		exit 1
+	fi
+	echo "${AMIIBO_API_KEY}"
+}
+
+SERVER="151.80.162.73 22403"
+
+if ! which nc &>/dev/null; then
+	echo "Netcat not detected. Please install or configure netcat and retry." >&2
+	exit 1
+fi
+
+if [ $# -ge 1 -a $# -le 3 ]; then
+	INPUT=-
+	if [ $# -ge 2 ]; then
+		INPUT="$2"
+	fi
+
+	if [ $# -ge 3 ]; then
+		OUTPUT="$3"
+		OUTPUTTMP=false
+	else
+		OUTPUT=$(mktemp)
+		OUTPUTTMP=true
+	fi
+else
+	usage
+fi
+
+case "$1" in
+	e)
+		APIKEY=$(loadapikey)
+		{
+			# Operation is encryption. Note this can't overlap with decryption as dumps begin with 0x04 (NXP manufacturer code)
+			echo -n "E"
+
+			# Now write authentication token
+			gentoken "${APIKEY}" 
+
+			cat "$INPUT"
+		} | nc $SERVER > "$OUTPUT"
+		;;
+
+	d)
+		cat "$INPUT" | nc $SERVER > "$OUTPUT"
+		;;
+
+	*)
+		if $OUTPUTTMP; then
+			rm "$OUTPUT"
+		fi
+		usage
+		;;
+esac
+
+if [ $(fsize "$OUTPUT") == 2 ]; then
+	ERR=$(cat "$OUTPUT")
+
+	echo -n "Error $ERR: " >&2
+	case "$ERR" in
+		03)
+			echo "invalid dump size" >&2
+			;;
+		06)
+			echo "invalid dump signature" >&2
+			;;
+		80)
+			echo "unsupported command (server's not up-to-date?)" >&2
+			;;
+		81)
+			echo "invalid authentication token" >&2
+			;;
+		*)
+			echo "unknown error (server on fire?)" >&2
+			;;
+	esac
+	rm "$OUTPUT"
+	exit 1
+fi
+
+if $OUTPUTTMP; then
+	cat "$OUTPUT"
+	rm "$OUTPUT"
+fi
+
+exit 0
---
+++ network/amiisrv.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+cd $(dirname "$0")
+
+log_err() {
+	echo "[!!!] $1" >>/var/log/amiibo.log
+}
+
+log_debug() {
+	echo "[---] $1" >>/var/log/amiibo.log
+}
+
+checkauth() {
+	VERSION=$(head -c1)
+
+	# Check version
+	if [ "$VERSION" != "1" ]; then
+		log_err "Invalid version: " $(echo $VERSION | od -An -tx1)
+		return 1
+	fi
+
+	# Now read first 16 bytes which are hexadecimal characters. Those are the fixed
+	USER=$(head -c16 | tr [a-f] [A-F])
+	if ! [[ "$USER" =~ [A-F0-9]{16} ]]; then
+		log_err "Illegal user: " $(echo $USER | od -An -tx1)
+		return 1
+	fi
+	log_debug "User: $USER"
+
+	# Check username and load it so we can use its AMIIBO_API_KEY
+	if [ ! -f auth/$USER.dat ]; then
+		log_err "Unknown user: $USER"
+		return 1
+	fi
+	. auth/$USER.dat
+
+	# Read next 16 hex chars which are the time-based token
+	CLITOKEN=$(head -c16 | tr [A-F] [a-f])
+	if ! [[ "$CLITOKEN" =~ [a-f0-9]{16} ]]; then
+		log_err "Illegal token: " $(echo $CLITOKEN | od -An -tx1)
+		return 1
+	fi
+
+	# Tick is as in TOTP: unix time / 30
+	SRVTICK=$(($(date -u +%s)/30))
+	log_debug "Local tick: $SRVTICK"
+
+	# Now attempt to match time within a certain offset. Computer's time should be NTP-updated and therefore very precise, so no need to check anything outside +-1.
+	for OFFSET in 0 -1 1; do
+		TICK=$(($SRVTICK+$OFFSET))
+		SRVTOKEN=$(echo -n "$TICK-${AMIIBO_API_KEY:16:16}" | sha256sum | head -c16)
+
+		if [ "$SRVTOKEN" == "$CLITOKEN" ]; then
+			log_debug "Token for $USER valid"
+			return 0
+		fi
+	done
+
+	log_err "Out-of-date token: $CLITOKEN"
+	return 1
+}
+
+OP=$(head -c1 | od -tx1 -An)
+
+case $OP in
+	\ 04)
+		log_debug "Requested decoding"
+		(echo -ne "\x04" && cat) | tee /tmp/request | ./amiitool -d -k retail_unfixed.bin 2>/dev/null
+		RET=$?
+		;;
+
+	\ 45)
+		log_debug "Requested encoding"
+		if checkauth; then
+			./amiitool -e -k retail_unfixed.bin 2>/dev/null
+			RET=$?
+		else
+			RET=81
+		fi
+		;;
+
+	*)
+		log_err "Unknown command: $OP"
+		RET=80
+		;;
+esac
+
+if [ $RET -ne 0 ]; then
+	printf %02d $RET
+fi
