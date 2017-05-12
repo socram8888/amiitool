38a8858470eeb1da74c98a0bedfa0cfd2185cdb4
Stany MARCEL <stanypub@gmail.com>
2017-05-13 00:44:15 +0200

    Add copy function
    Signed-off-by: Stany MARCEL <stanypub@gmail.com>

--- amiitool.c
+++ amiitool.c
@@ -22,11 +22,13 @@ void usage() {
 		"amiitool build %i (commit %s-%08x)\n"
 		"by Marcos Del Sol Vives <marcos@dracon.es>\n"
 		"\n"
-		"Usage: %s (-e|-d) -k keyfile [-i input] [-o output]\n"
+		"Usage: %s (-e|-d|-c) -k keyfile [-i input] [-s input2] [-o output]\n"
 		"   -e encrypt and sign amiibo\n"
 		"   -d decrypt and test amiibo\n"
+		"   -c decrypt, copy AppData and encrypt amiibo\n"
 		"   -k key set file. For retail amiibo, use \"retail unfixed\" key set\n"
 		"   -i input file. If not specified, stdin will be used.\n"
+		"   -s input save file, save from this file will replace input file ones.\n"
 		"   -o output file. If not specified, stdout will be used.\n"
 		"   -l decrypt files with invalid signatures.\n",
 		nfc3d_version_build(), nfc3d_version_fork(), nfc3d_version_commit(), self
@@ -37,20 +39,25 @@ int main(int argc, char ** argv) {
 	self = argv[0];
 
 	char * infile = NULL;
+        char * savefile = NULL;
 	char * outfile = NULL;
 	char * keyfile = NULL;
 	char op = '\0';
 	bool lenient = false;
 
 	char c;
-	while ((c = getopt(argc, argv, "edi:o:k:l")) != -1) {
+	while ((c = getopt(argc, argv, "edci:s:o:k:l")) != -1) {
 		switch (c) {
 			case 'e':
 			case 'd':
+			case 'c':
 				op = c;
 				break;
 			case 'i':
 				infile = optarg;
+				break;
+			case 's':
+				savefile = optarg;
 				break;
 			case 'o':
 				outfile = optarg;
@@ -96,15 +103,49 @@ int main(int argc, char ** argv) {
 	}
 	fclose(f);
 
+
 	if (op == 'e') {
 		nfc3d_amiibo_pack(&amiiboKeys, original, modified);
-	} else {
+	} else if (op == 'd') {
 		if (!nfc3d_amiibo_unpack(&amiiboKeys, original, modified)) {
 			fprintf(stderr, "!!! WARNING !!!: Tag signature was NOT valid\n");
 			if (!lenient) {
 				return 6;
 			}
 		}
+	} else { /* copy */
+		uint8_t plain_base[NFC3D_AMIIBO_SIZE];
+		uint8_t plain_save[NFC3D_AMIIBO_SIZE];
+
+		if (!nfc3d_amiibo_unpack(&amiiboKeys, original, plain_base)) {
+			fprintf(stderr, "!!! WARNING !!!: Tag signature was NOT valid\n");
+			if (!lenient) {
+				return 6;
+			}
+		}
+		if (savefile) {
+			f = fopen(savefile, "rb");
+			if (!f) {
+				fprintf(stderr, "Could not open save file: %s (%d)\n", strerror(errno), errno);
+				return 3;
+			}
+		}
+		size_t readPages = fread(original, 4, NTAG215_SIZE / 4, f);
+		if (readPages < NFC3D_AMIIBO_SIZE / 4) {
+			fprintf(stderr, "Could not read from save: %s (%d)\n", strerror(errno), errno);
+			return 3;
+		}
+		fclose(f);
+
+		if (!nfc3d_amiibo_unpack(&amiiboKeys, original, plain_save)) {
+			fprintf(stderr, "!!! WARNING !!!: Tag signature was NOT valid\n");
+			if (!lenient) {
+				return 6;
+			}
+		}
+
+		nfc3d_amiibo_copy_app_data(plain_base, plain_save);
+		nfc3d_amiibo_pack(&amiiboKeys, plain_base, modified);
 	}
 
 	f = stdout;
